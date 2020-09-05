a27cc74ce435c4edd41c192dc28212391037fec7
Donald Wu <yeukfei02@gmail.com>
2020-09-05 23:54:06 +0800

    feature/create-project-structure-#1: update cron.py

--- src/cron/cron.py
+++ src/cron/cron.py
@@ -27,8 +27,10 @@ def make_celery(app):
             'schedule': crontab(minute="*")
         }
     }
+    app.config['CELERY_TRACK_STARTED'] = True
+    app.config['CELERY_SEND_EVENTS'] = True
 
-    celery = Celery(app.import_name)
+    celery = Celery(app.import_name, backend=app.config['CELERY_RESULT_BACKEND'], broker=app.config['CELERY_BROKER_URL'])
     celery.conf.update(app.config)
 
     TaskBase = celery.Task
