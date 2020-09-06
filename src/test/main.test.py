f8964bf812ad22589e0d0fc4210f1f76cb241144
Donald Wu <yeukfei02@gmail.com>
2020-09-06 12:55:58 +0800

    feature/create-project-structure-#1: add test case

--- src/test/main.test.py
+++ src/test/main.test.py
@@ -1,9 +1,116 @@
 import unittest
+from datetime import datetime
+
+import os
+import sys
+sys.path.insert(1, os.getcwd())
+
+from src.models.TropicalCyclone import *
+from app import db
 
 class MainTest(unittest.TestCase):
-    def test(self):
-        result = 1 + 1
-        self.assertEqual(result, 2)
+    id = 0
+    
+    @classmethod
+    def setUpClass(cls):
+        last_tropical_cyclone = TropicalCyclone.query.order_by(TropicalCyclone.tropical_cyclone_id.desc()).first()
+        if last_tropical_cyclone:
+            cls.id = last_tropical_cyclone.tropical_cyclone_id
+
+    def test_001_create_tropical_cyclone(self):
+        print('test_001_create_tropical_cyclone start')
+
+        place = "test"
+        description_text = "test2"
+        image = "test3"
+        now = datetime.now()
+
+        tropical_cyclone = TropicalCyclone(place, description_text, image, now, now)
+
+        self.assertTrue(tropical_cyclone is not None)
+
+        if tropical_cyclone:
+            self.assertIsNotNone(tropical_cyclone.place)
+            self.assertIsNotNone(tropical_cyclone.description_text)
+            self.assertIsNotNone(tropical_cyclone.image)
+            self.assertIsNotNone(str(tropical_cyclone.created_by))
+            self.assertIsNotNone(str(tropical_cyclone.updated_by))
+        
+        print('test_001_create_tropical_cyclone end')
+
+    def test_002_get_tropical_cyclone(self):
+        print('test_002_get_tropical_cyclone start')
+
+        tropical_cyclone_list = TropicalCyclone.query.order_by(TropicalCyclone.tropical_cyclone_id).all()
+
+        self.assertTrue(tropical_cyclone_list is not None)
+
+        if tropical_cyclone_list:
+            for tropical_cyclone in tropical_cyclone_list:
+                self.assertIsNotNone(tropical_cyclone.place)
+                self.assertIsNotNone(tropical_cyclone.description_text)
+                self.assertIsNotNone(tropical_cyclone.image)
+                self.assertIsNotNone(str(tropical_cyclone.created_by))
+                self.assertIsNotNone(str(tropical_cyclone.updated_by))
+        
+        print('test_002_get_tropical_cyclone end')
+
+    def test_003_get_tropical_cyclone_by_id(self):
+        print('test_003_get_tropical_cyclone_by_id start')
+
+        tropical_cyclone = TropicalCyclone.query.filter_by(tropical_cyclone_id=self.id).first()
+
+        self.assertTrue(tropical_cyclone is not None)
+
+        if tropical_cyclone:
+            self.assertIsNotNone(tropical_cyclone.place)
+            self.assertIsNotNone(tropical_cyclone.description_text)
+            self.assertIsNotNone(tropical_cyclone.image)
+            self.assertIsNotNone(str(tropical_cyclone.created_by))
+            self.assertIsNotNone(str(tropical_cyclone.updated_by))
+        
+        print('test_003_get_tropical_cyclone_by_id end')
+
+    def test_004_update_tropical_cyclone_by_id(self):
+        print('test_004_update_tropical_cyclone_by_id start')
+
+        tropical_cyclone = TropicalCyclone.query.filter_by(tropical_cyclone_id=self.id).first()
+
+        self.assertTrue(tropical_cyclone is not None)
+
+        if tropical_cyclone:
+            place = "aaa"
+            description_text = "bbb"
+            image = "ccc"
+            now = datetime.now()
+
+            tropical_cyclone = TropicalCyclone(place, description_text, image, now, now)
+            tropical_cyclone.place = place
+            tropical_cyclone.description_text = description_text
+            tropical_cyclone.image = image
+
+            db.session.commit()
+
+            self.assertIsNotNone(tropical_cyclone.place)
+            self.assertIsNotNone(tropical_cyclone.description_text)
+            self.assertIsNotNone(tropical_cyclone.image)
+            self.assertIsNotNone(str(tropical_cyclone.created_by))
+            self.assertIsNotNone(str(tropical_cyclone.updated_by))
+        
+        print('test_004_update_tropical_cyclone_by_id end')
+
+    def test_005_delete_tropical_cyclone_by_id(self):
+        print('test_005_delete_tropical_cyclone_by_id start')
+
+        tropical_cyclone = TropicalCyclone.query.filter_by(tropical_cyclone_id=self.id).first()
+
+        self.assertTrue(tropical_cyclone is not None)
+
+        if tropical_cyclone:
+            db.session.delete(tropical_cyclone)
+            db.session.commit()
+
+        print('test_005_delete_tropical_cyclone_by_id end')
 
 if __name__ == '__main__':
     unittest.main()
