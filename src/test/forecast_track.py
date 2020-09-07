72777a3a87e3072b1fd39667c2fe2d0b3c105efb
Donald Wu <yeukfei02@gmail.com>
2020-09-07 17:14:49 +0800

    fix-code: add more test case

---
+++ src/test/forecast_track.py
@@ -0,0 +1,132 @@
+import unittest
+from datetime import datetime
+
+import os
+import sys
+sys.path.insert(1, os.getcwd())
+
+from src.models.ForecastTrack import *
+from app import db
+
+class ForecastTrackTest(unittest.TestCase):
+    id = 0
+    
+    @classmethod
+    def setUpClass(cls):
+        last_forecast_track = ForecastTrack.query.order_by(ForecastTrack.forecast_track_id.desc()).first()
+        if last_forecast_track:
+            cls.id = last_forecast_track.forecast_track_id
+
+    def test_001_create_forecast_track(self):
+        print('test_001_create_forecast_track start')
+
+        description_id = "AAABBB123"
+        forecast_hour = 21
+        latitude = 100.11
+        longitude = 10.22
+        intensity = 30
+        now = datetime.now()
+
+        forecast_track = ForecastTrack(description_id, forecast_hour, latitude, longitude, intensity, now, now)
+        db.session.add(forecast_track)
+        db.session.commit()
+
+        self.assertTrue(forecast_track is not None)
+
+        if forecast_track:
+            self.assertIsNotNone(forecast_track.description_id)
+            self.assertIsNotNone(forecast_track.forecast_hour)
+            self.assertIsNotNone(forecast_track.latitude)
+            self.assertIsNotNone(forecast_track.longitude)
+            self.assertIsNotNone(forecast_track.intensity)
+            self.assertIsNotNone(str(forecast_track.created_by))
+            self.assertIsNotNone(str(forecast_track.updated_by))
+        
+        print('test_001_create_forecast_track end')
+
+    def test_002_get_forecast_track(self):
+        print('test_002_get_forecast_track start')
+
+        forecast_track_list = ForecastTrack.query.order_by(ForecastTrack.forecast_track_id).all()
+
+        self.assertTrue(forecast_track_list is not None)
+
+        if forecast_track_list:
+            for forecast_track in forecast_track_list:
+                self.assertIsNotNone(forecast_track.description_id)
+                self.assertIsNotNone(forecast_track.forecast_hour)
+                self.assertIsNotNone(forecast_track.latitude)
+                self.assertIsNotNone(forecast_track.longitude)
+                self.assertIsNotNone(forecast_track.intensity)
+                self.assertIsNotNone(str(forecast_track.created_by))
+                self.assertIsNotNone(str(forecast_track.updated_by))
+        
+        print('test_002_get_forecast_track end')
+
+    def test_003_get_forecast_track_by_id(self):
+        print('test_003_get_forecast_track_by_id start')
+
+        forecast_track = ForecastTrack.query.filter_by(forecast_track_id=self.id).first()
+
+        self.assertTrue(forecast_track is not None)
+
+        if forecast_track:
+            self.assertIsNotNone(forecast_track.description_id)
+            self.assertIsNotNone(forecast_track.forecast_hour)
+            self.assertIsNotNone(forecast_track.latitude)
+            self.assertIsNotNone(forecast_track.longitude)
+            self.assertIsNotNone(forecast_track.intensity)
+            self.assertIsNotNone(str(forecast_track.created_by))
+            self.assertIsNotNone(str(forecast_track.updated_by))
+        
+        print('test_003_get_forecast_track_by_id end')
+
+    def test_004_update_forecast_track_by_id(self):
+        print('test_004_update_forecast_track_by_id start')
+
+        forecast_track = ForecastTrack.query.filter_by(forecast_track_id=self.id).first()
+
+        self.assertTrue(forecast_track is not None)
+
+        if forecast_track:
+            description_id = "CCCDDD556"
+            forecast_hour = 29
+            latitude = 8.11
+            longitude = 88.22
+            intensity = 99
+            now = datetime.now()
+
+            forecast_track.description_id = description_id
+            forecast_track.forecast_hour = forecast_hour
+            forecast_track.latitude = latitude
+            forecast_track.longitude = longitude
+            forecast_track.intensity = intensity
+            forecast_track.updated_by = now
+
+            db.session.commit()
+
+            self.assertIsNotNone(forecast_track.description_id)
+            self.assertIsNotNone(forecast_track.forecast_hour)
+            self.assertIsNotNone(forecast_track.latitude)
+            self.assertIsNotNone(forecast_track.longitude)
+            self.assertIsNotNone(forecast_track.intensity)
+            self.assertIsNotNone(str(forecast_track.created_by))
+            self.assertIsNotNone(str(forecast_track.updated_by))
+        
+        print('test_004_update_forecast_track_by_id end')
+
+    def test_005_delete_forecast_track_by_id(self):
+        print('test_005_delete_forecast_track_by_id start')
+
+        forecast_track = ForecastTrack.query.filter_by(forecast_track_id=self.id).first()
+
+        self.assertTrue(forecast_track is not None)
+
+        if forecast_track:
+            db.session.delete(forecast_track)
+            db.session.commit()
+
+        print('test_005_delete_forecast_track_by_id end')
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ src/test/track_hisotry.py
@@ -0,0 +1,132 @@
+import unittest
+from datetime import datetime
+
+import os
+import sys
+sys.path.insert(1, os.getcwd())
+
+from src.models.TrackHistory import *
+from app import db
+
+class TrackHistoryTest(unittest.TestCase):
+    id = 0
+    
+    @classmethod
+    def setUpClass(cls):
+        last_track_history = TrackHistory.query.order_by(TrackHistory.track_history_id.desc()).first()
+        if last_track_history:
+            cls.id = last_track_history.track_history_id
+
+    def test_001_create_track_history(self):
+        print('test_001_create_track_history start')
+
+        description_id = "AAABBB123"
+        synoptic_time = "2020-03-21 15:30"
+        latitude = 22.22
+        longitude = 33.33
+        intensity = 50
+        now = datetime.now()
+
+        track_history = TrackHistory(description_id, synoptic_time, latitude, longitude, intensity, now, now)
+        db.session.add(track_history)
+        db.session.commit()
+
+        self.assertTrue(track_history is not None)
+
+        if track_history:
+            self.assertIsNotNone(track_history.description_id)
+            self.assertIsNotNone(track_history.synoptic_time)
+            self.assertIsNotNone(track_history.latitude)
+            self.assertIsNotNone(track_history.longitude)
+            self.assertIsNotNone(track_history.intensity)
+            self.assertIsNotNone(str(track_history.created_by))
+            self.assertIsNotNone(str(track_history.updated_by))
+        
+        print('test_001_create_track_history end')
+
+    def test_002_get_track_history(self):
+        print('test_002_get_track_history start')
+
+        track_history_list = TrackHistory.query.order_by(TrackHistory.track_history_id).all()
+
+        self.assertTrue(track_history_list is not None)
+
+        if track_history_list:
+            for track_history in track_history_list:
+                self.assertIsNotNone(track_history.description_id)
+                self.assertIsNotNone(track_history.synoptic_time)
+                self.assertIsNotNone(track_history.latitude)
+                self.assertIsNotNone(track_history.longitude)
+                self.assertIsNotNone(track_history.intensity)
+                self.assertIsNotNone(str(track_history.created_by))
+                self.assertIsNotNone(str(track_history.updated_by))
+        
+        print('test_002_get_track_history end')
+
+    def test_003_get_track_history_by_id(self):
+        print('test_003_get_track_history_by_id start')
+
+        track_history = TrackHistory.query.filter_by(track_history_id=self.id).first()
+
+        self.assertTrue(track_history is not None)
+
+        if track_history:
+            self.assertIsNotNone(track_history.description_id)
+            self.assertIsNotNone(track_history.synoptic_time)
+            self.assertIsNotNone(track_history.latitude)
+            self.assertIsNotNone(track_history.longitude)
+            self.assertIsNotNone(track_history.intensity)
+            self.assertIsNotNone(str(track_history.created_by))
+            self.assertIsNotNone(str(track_history.updated_by))
+        
+        print('test_003_get_track_history_by_id end')
+
+    def test_004_update_track_history_by_id(self):
+        print('test_004_update_track_history_by_id start')
+
+        track_history = TrackHistory.query.filter_by(track_history_id=self.id).first()
+
+        self.assertTrue(track_history is not None)
+
+        if track_history:
+            description_id = "EEEHHH123"
+            synoptic_time = "2025-05-25 15:30"
+            latitude = 55.5
+            longitude = 66.666
+            intensity = 880
+            now = datetime.now()
+
+            track_history.description_id = description_id
+            track_history.synoptic_time = synoptic_time
+            track_history.latitude = latitude
+            track_history.longitude = longitude
+            track_history.intensity = intensity
+            track_history.updated_by = now
+
+            db.session.commit()
+
+            self.assertIsNotNone(track_history.description_id)
+            self.assertIsNotNone(track_history.synoptic_time)
+            self.assertIsNotNone(track_history.latitude)
+            self.assertIsNotNone(track_history.longitude)
+            self.assertIsNotNone(track_history.intensity)
+            self.assertIsNotNone(str(track_history.created_by))
+            self.assertIsNotNone(str(track_history.updated_by))
+        
+        print('test_004_update_track_history_by_id end')
+
+    def test_005_delete_track_history_by_id(self):
+        print('test_005_delete_track_history_by_id start')
+
+        track_history = TrackHistory.query.filter_by(track_history_id=self.id).first()
+
+        self.assertTrue(track_history is not None)
+
+        if track_history:
+            db.session.delete(track_history)
+            db.session.commit()
+
+        print('test_005_delete_track_history_by_id end')
+
+if __name__ == '__main__':
+    unittest.main()
